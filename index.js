8572a199fbae5b30edee576f36e0b2d95ea72c82
jpmorais <jpmorais@gmail.com>
2023-02-22 13:40:26 -0300

    resposta

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 require("dotenv").config()
 const express = require("express")
 const server = express()
+const axios = require("axios")
 
 server.use(express.json())
-server.post('/hook', (req, res) => {
+server.post('/hook', async (req, res) => {
+    // pega as informações necessárias
+    const {message} = req.body
+    const {from} = message
+
+    const mensagemResposta = {
+        from: "stream-sweatshirt",
+        to: from,
+        contents: [
+            {
+                type: "text",
+                text: "mensagem recebida!"
+            }
+        ]
+    }
+
+    const retorno = await axios.post('https://api.zenvia.com/v2/channels/whatsapp/messages', {
+        "X-API-TOKEN":process.env.ZEN_APIKEY
+    }, mensagemResposta)
+
+    console.log(`Retorno ${retorno.data}`)
+
     console.log(req.body)
     res.send(req.body)
 })
